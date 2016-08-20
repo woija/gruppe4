06b32019d9d91acb5b7de4d4ae32bb1d5f601e97
Sabineverning <sabineverning13@Sabines-MacBook-Pro.local>
2016-08-20 14:16:24 +0200

    Plots

--- ExamPlots.R
+++ ExamPlots.R
@@ -7,34 +7,38 @@ library("ggplot2")
 library("tidyr")
 library("stringr")
 
+londata = read.csv("londata1.csv", sep = ";")
+Udddata = read.csv("optagelse3.csv", sep = ";")
 
-
-londata = import("londata1.csv")
-Udddata = import("optagelse3.csv")
-
-Udddata.1 = Udddata %>%
+Udddata = Udddata %>%
   mutate(OptMK = round(OptMaend / OptKvinder,2),
          TotalOpt = OptMaend + OptKvinder,
          TotalAng = AngMaend + AngKvinder,
          AngMK = round(AngMaend / AngKvinder,2)  ) %>%
   arrange(-TotalOpt, -TotalAng, - AngMK, -OptMK)
 
-Udddata.2 = Udddata.1 %>% 
+Udddata.1 = Udddata %>% 
   left_join(londata, by = "Placering")
 
-Udddata.3 = Udddata.2 %>%
+Udddata.2 = Udddata.1 %>%
   filter( !is.na(GnsIndkomst) )
 
-data = Udddata.3 %>%
+Udddata.2 = Udddata.2 %>%
+  mutate(Kvotient = as.numeric(gsub(",",".",data$Kvotient))) %>%
+  filter(!is.na(AngMaend) | !is.na(AngKvinder) ) 
+
+Udddata.2 = Udddata.2 %>%
   mutate( KvindeAndOpt = round(OptKvinder / TotalOpt, 2),
           KvindeAndAng = round(AngKvinder / TotalAng, 2)
   )
 
-Uni.2 = Udddata.3 %>%
+
+
+### PLOT AF KØN PÅ INSTITUTIONER
+Uni.2 = Udddata.2 %>%
   group_by(InstNavn) %>%
   summarise(AntalM =sum(round(AngMaend/4,0), na.rm =TRUE), AntalK =sum(round(AngKvinder/4,0), na.rm=TRUE)) 
 
-### PLOT AF KØN PÅ INSTITUTIONER
 Uni.2gather = Uni.2 %>%
   gather(key = gender, 
          value = frequency,
@@ -42,10 +46,12 @@ Uni.2gather = Uni.2 %>%
 
 ggplot(data=Uni.2gather, aes(x=reorder(InstNavn, frequency), y=frequency, fill=gender)) +
   geom_bar(stat="identity") +
-  coord_flip() + ylab("Total") + xlab("") 
+  coord_flip() + ylab("Total") + xlab("") +
+  ggtitle("Fordeling af køn på danske uddannelse institutioner") + 
+  scale_fill_brewer(palette = "Set1")
 
 ### PLOT AF KØN OG TOPLØN
-Uni.3 = Udddata.3 %>%
+Uni.3 = Udddata.2 %>%
   group_by(GnsIndkomst, Uddannelse) %>%
   summarise(Mænd =sum(round(AngMaend/4,0), na.rm =TRUE), Kvinder =sum(round(AngKvinder/4,0), na.rm=TRUE)) 
 
@@ -65,29 +71,11 @@ ggplot(data=Uni.3gather, aes(x=reorder(GnsIndkomst, Uddannelse), y=frequency, fi
 scale_x_discrete(breaks=c("801156", "805757", "809791", "847594", "849602", "853619", "909823", "926244", "1052373", "1180098"),
                  labels=c("Civil.ing.kemi, 801.156", "Civil.ing.elektro, 805.757", "Cand.oecon, 809.791", "Erhvervsøkonomi mat, 847.594", 
                           "Jura, 849.602", "Civil.ing.maskin, 853.619", "Matematikøkonomi, 909.823", "Læge , 926.244", 
-                          "Cand.polit, 1.052.372", "Forsikringsmatematik, 1.180.098"))
+                          "Cand.polit, 1.052.372", "Forsikringsmatematik, 1.180.098")) +
+  ggtitle("Top 10 af højeste lønninger og gennesnits ansøgertallet for disse 
+          uddannelser, 2013-2016") + 
+  scale_fill_brewer(palette = "Set1")
 
-### PLOT AF KØN OG KVOTIENTER
-Uni.4 = Udddata.3 %>%
-  group_by(Kvotient, OptNr) %>%
-  summarise(Mænd =sum(round(AngMaend/4,0), na.rm =TRUE), Kvinder =sum(round(AngKvinder/4,0), na.rm=TRUE)) 
-  
-
-Uni.4$Kvotient = as.numeric(Uni.4$Kvotient)
-
-  Uni.4 = Uni.4 %>%
-arrange(desc(Kvotient)) %>%
-  head(n=10)
-  
-
-Uni.4gather = Uni.4 %>%
-  gather(key = Køn, 
-         value = frequency,
-         -Kvotient, - OptNr)
-
-ggplot(data=Uni.4gather, aes(x=reorder(OptNr, Kvotient), y=frequency, fill=Køn)) +
-  geom_bar(stat="identity") +
-  coord_flip() + ylab("Total") + xlab("") 
 
 
 ### GNS. TOP 10 MEST SØGTE MÆND
@@ -102,9 +90,13 @@ Uni.5 = Uni.5 %>%
 Uni.5$OptNr = factor(Uni.5$OptNr)
 
 ggplot(data=Uni.5, aes(x=reorder(OptNr, Mænd), y= Mænd)) + geom_point() +
+  coord_flip() + ylab("Total") + xlab("") + 
   scale_x_discrete(breaks=c("13045", "22415", "30010", "17115", "22010", "13060", "22610", "10110", "10410", "13010"),
                    labels=c("Erhversøkonomi Projektledelse, CBS", "Jura, AU", "Arkitekt", "Læge, SDU", "Læge, AU", 
-                            "International Business, CBS", "Erhvervsøkonomi, AU", "Læge, KU", "Jura, KU", "Erhvervsøkonomi, CBS"))
+                            "International Business, CBS", "Erhvervsøkonomi, AU", "Læge, KU", "Jura, KU", "Erhvervsøkonomi, CBS")) + 
+ggtitle("Top 10 gennemsnitlig mest søgte uddannelse for 
+        mænd, 2013-2016") 
+ 
 
 ### GNS. TOP 10 MEST SØGTE KVINDER
 Uni.6 = Udddata.3 %>%
@@ -118,7 +110,10 @@ Uni.6 = Uni.6 %>%
 Uni.6$OptNr = factor(Uni.6$OptNr)
 
 ggplot(data=Uni.6, aes(x=reorder(OptNr, Kvinder), y= Kvinder)) + geom_point() + 
+  coord_flip() + ylab("Total") + xlab("") +
 scale_x_discrete(breaks=c("22415", "16020", "22420", "17115", "10140", "22010", "32010", "10410", "10368", "10110"),
                  labels=c("Jura, AU", "Human/teologi, RUC", "Psykologi, AU", "Læge, SDU", "Veterinærmedicin, KU", "Læge, AU", 
-                          "Designer Unika Design", "Jura, KU", "Psykologi, KU", "Læge, KU"))
+                          "Designer Unika Design", "Jura, KU", "Psykologi, KU", "Læge, KU")) + 
+ggtitle("Top 10 gennemsnitlig mest søgte uddannelse for 
+        kvinder, 2013-2016") 
 
---
+++ Køn og kvotienter.R
@@ -0,0 +1,59 @@
+library("rio")
+library("ggplot2")
+library("dplyr")
+library("stringr")
+library("ggvis")
+library("ggrepel")
+library("knitr")
+
+londata <- read.csv("londata1.csv", sep = ";")
+
+uddannelse <- read.csv("optagelse3.csv", sep = ";")
+
+# samler data
+data <- uddannelse %>%
+  left_join(londata, by = "Placering")
+
+data <- data %>%
+  filter(!is.na(GnsIndkomst))
+
+data <- data %>%
+  mutate(Kvotient = as.numeric(gsub(",",".",data$Kvotient))) %>%
+  filter(!is.na(AngMaend) | !is.na(AngKvinder) ) 
+
+
+#Kvindeandel i optag og ansøg
+data2 = data %>%
+  mutate(KvindeAndOpt = round(OptKvinder /(OptKvinder + OptMaend),2), 
+         KvindeAndAng = round(AngKvinder / (AngKvinder + AngMaend),2),
+         TotalOpt = OptKvinder + OptMaend,
+         TotalAng = AngKvinder + AngMaend)
+
+
+### PLOT AF KØN OG KVOTIENTER
+Uni.4 = data2 %>%
+  group_by(OptNr) %>%
+  summarise(Mænd =sum(round(AngMaend/4,0), na.rm =TRUE), Kvinder =sum(round(AngKvinder/4,0), na.rm=TRUE), Kvotient=mean(Kvotient)) 
+
+Uni.4 = Uni.4 %>%
+  arrange(desc(Kvotient)) %>%
+  head(n=10)
+
+
+Uni.4gather = Uni.4 %>%
+  gather(key = Køn, 
+         value = frequency,
+         -Kvotient, - OptNr)
+
+ggplot(data=Uni.4gather, aes(x=reorder(Kvotient, Køn), y=frequency, fill=Køn)) +
+  geom_bar(stat="identity") +
+  coord_flip() + ylab("Total") + xlab("") +
+scale_x_discrete(breaks=c("10.825", "10.85", "10.975", "11", "11.05", "11.15", "11.375", "11.675", "11.95", "12.125"), 
+                 labels=c("Statskundskab, KU, 10.83", "Medicin, AU, 10.85", "Antropologi, KU, 10.98", "Internation Shipping and Trade, CBS, 11", 
+                          "Erhvervsøkonomi Projektledelse, CBS, 11.05", "Medicin, KU, 11.15 ", "Psykologi, KU, 11.38", "International Business and Politics, CBS, 11.68", 
+                          "Cand.Scient u.n.a. Naturvidenskab , 11.95", "International Business, CBS, 12.13")) +
+  ggtitle("Top 10 gennemsnitlig højest kvotient og ansøger fordelt på køn") + 
+  scale_fill_brewer(palette = "Set1")
+
+
+
